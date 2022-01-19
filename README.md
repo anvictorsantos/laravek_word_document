4ea4f668d50acebd196f65cc4863fd5fc88cf87a
Antonio Pontes <anvictorsantos@gmail.com>
2022-01-19 20:42:44 -0300

    fix - adjustment in readme file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Observation
+
+This application was designed by this <a src="https://appdividend.com/2018/04/23/how-to-create-word-document-file-in-laravel/" target="_blank">article</a>.
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
---
+++ resources/views/createdocument.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Create Word File in Laravel</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container">
+      <h2>Create Word File in Laravel</h2><br/>
+      <form method="post" action="{{url('store')}}">
+        @csrf
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="Name">Name:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Email">Email:</label>
+              <input type="text" class="form-control" name="email">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Number">Phone Number:</label>
+              <input type="text" class="form-control" name="number">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </div>
+      </form>
+   </div>
+  </body>
+</html>
